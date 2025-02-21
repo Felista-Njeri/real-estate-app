3e92350b182cf1badf722c9bbf5f48557f29b07d
Felista-Njeri <njeerikamau2@gmail.com>
2025-02-21 20:31:31 +0300

    updated the rainbow config

--- frontend-vite/src/main.tsx
+++ frontend-vite/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { WagmiProvider } from "wagmi";
-import { wagmiconfig } from "../wagmiconfig.ts";
+//import { wagmiconfig } from "../wagmiconfig.ts";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import '@rainbow-me/rainbowkit/styles.css';
 import { getDefaultConfig, RainbowKitProvider} from '@rainbow-me/rainbowkit';
@@ -9,7 +9,7 @@ import { sepolia, liskSepolia} from 'wagmi/chains';
 import "./index.css";
 import App from "./App.tsx";
 
-const config = getDefaultConfig({
+const rainbowconfig = getDefaultConfig({
   appName: 'My RainbowKit App',
   projectId: 'YOUR_PROJECT_ID',
   chains: [ sepolia, liskSepolia ],
@@ -19,7 +19,7 @@ const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <WagmiProvider config={wagmiconfig}>
+    <WagmiProvider config={rainbowconfig}>
       <QueryClientProvider client={queryClient}>
         <RainbowKitProvider>
           <App />
