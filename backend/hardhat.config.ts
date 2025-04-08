196d007f4088f74a2b6caf85d916bff28db03923
Felista-Njeri <njeerikamau2@gmail.com>
2025-04-08 03:00:09 +0300

    Update hardhat config

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -5,8 +5,7 @@ import "hardhat-deploy";
 import "dotenv/config";
 
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "api-key";
-//const SEPOLIA_ETHERSCAN_API_KEY = process.env.SEPOLIA_ETHERSCAN_API_KEY || "api-key";
-const WALLET_PRIVATE_KEY = process.env.WALLET_PRIVATE_KEY;
+const WALLET_PRIVATE_KEY = process.env.WALLET_PRIVATE_KEY || "";
 const MNEMONIC = process.env.MNEMONIC || "your mnemonic"
 
 const config: HardhatUserConfig = {
