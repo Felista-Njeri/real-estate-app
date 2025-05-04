6aacfc3436ed54fc93977faeb392529acab69918
Felista-Njeri <njeerikamau2@gmail.com>
2025-05-05 01:22:32 +0300

    types

---
+++ frontend-vite/src/types/index.ts
@@ -0,0 +1,27 @@
+export interface Property {
+    propertyId: number;
+    metadataCID: string
+    tokenPrice: bigint;
+    tokensSold: bigint;
+    totalDividends: bigint;
+    totalTokens: bigint;
+    owner: string;
+    isActive: boolean;
+}
+  
+export interface PropertyMetaData {
+    propertyName: string,
+    location: string,
+    description:string,
+    images: string
+}
+
+export interface CombinedProperty extends PropertyMetaData {
+    propertyId: number;
+    totalTokens: bigint;
+    tokenPrice: bigint;
+    totalDividends: bigint;
+    owner: string;
+    isActive: boolean;
+    metadataCID: string;
+}
