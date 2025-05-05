4332f3341564b3e189175319ffd56788020974f9
Felista-Njeri <njeerikamau2@gmail.com>
2025-05-06 01:57:27 +0300

    refactor recent listings

--- frontend-vite/src/pages/Home.tsx
+++ frontend-vite/src/pages/Home.tsx
@@ -1,14 +1,14 @@
 import CallToAction from "@/reactcomponents/CallToAction";
 import Features from "@/reactcomponents/Features";
 import Hero from "@/reactcomponents/Hero";
-import RecentListings from "@/reactcomponents/RecentListings";
+import Marketplace from "./Marketplace";
 
 function Home() {
   return (
     <>
       <Hero />
       <Features />
-      <RecentListings />
+      <Marketplace />
       <CallToAction />
     </>
   );
