8e73d5a892d40abaacef672e5f37dcca3503a8fd
Felista-Njeri <njeerikamau2@gmail.com>
2025-06-15 11:38:07 +0300

    removed hardhat configuration from wagmi config

--- frontend-vite/wagmiconfig.ts
+++ frontend-vite/wagmiconfig.ts
@@ -1,35 +1,34 @@
 import { http, createConfig } from 'wagmi'
-import { liskSepolia, mainnet, sepolia } from 'wagmi/chains'
-import { defineChain } from 'viem';
+import { baseSepolia, mainnet, sepolia } from 'wagmi/chains'
+// import { defineChain } from 'viem';
 
-const hardhat = defineChain({
-  id: 1337,
-  name: 'Hardhat',
-  network: 'hardhat',
-  nativeCurrency: {
-    name: 'Ether',
-    symbol: 'ETH',
-    decimals: 18,
-  },
-  rpcUrls: {
-    default: {
-      http: ['http://127.0.0.1:8545'],
-    },
-    public: {
-      http: ['http://127.0.0.1:8545'],
-    },
-  },
-  testnet: true,
-});
+// const hardhat = defineChain({
+//   id: 1337,
+//   name: 'Hardhat',
+//   network: 'hardhat',
+//   nativeCurrency: {
+//     name: 'Ether',
+//     symbol: 'ETH',
+//     decimals: 18,
+//   },
+//   rpcUrls: {
+//     default: {
+//       http: ['http://127.0.0.1:8545'],
+//     },
+//     public: {
+//       http: ['http://127.0.0.1:8545'],
+//     },
+//   },
+//   testnet: true,
+// });
 
 
 
 export const wagmiconfig = createConfig({
-  chains: [mainnet, sepolia, liskSepolia, hardhat],
+  chains: [mainnet, sepolia, baseSepolia],
   transports: {
     [mainnet.id]: http(),
     [sepolia.id]: http(),
-    [liskSepolia.id]: http(),
-    [hardhat.id]: http(), 
+    [baseSepolia.id]: http(),
   },
 })
